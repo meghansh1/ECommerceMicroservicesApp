a53fd41b2bf93a1885994be1da9c0eed9c4c00b8
Meghansh Saxena <meghanshsaxena1@gmail.com>
2026-06-24 00:37:44 +0530

    Enhance product domain model and introduce DTO contracts

--- src/ProductService/Domain/Entities/Product.cs
+++ src/ProductService/Domain/Entities/Product.cs
@@ -13,4 +13,12 @@ public class Product
     public int StockQuantity { get; set; }
 
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    
+    public Guid CategoryId { get; set; }
+
+    public string SKU { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; } = true;
+
+    public DateTime UpdatedDate { get; set; }
 }
--- src/ProductService/obj/Debug/net8.0/ProductService.AssemblyInfo.cs
+++ src/ProductService/obj/Debug/net8.0/ProductService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71075e1e0c5375a746cbf4b4239477822b6aa8ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d17365256b2ebd39c0c88d8782b83e59a7ce1d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
