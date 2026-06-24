bd072b4cca6e0c8084977c7a6d598eb0b577c1f5
Meghansh Saxena <meghanshsaxena1@gmail.com>
2026-06-25 01:21:22 +0530

    Add EF Core DbContext and entity configurations

---
+++ src/ProductService/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductService.Domain.Entities;
+
+namespace ProductService.Persistence.Configurations;
+
+public class CategoryConfiguration :
+    IEntityTypeConfiguration<Category>
+{
+    public void Configure(
+        EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+               .HasMaxLength(200)
+               .IsRequired();
+
+        builder.Property(x => x.Description)
+               .HasMaxLength(1000);
+    }
+}
---
+++ src/ProductService/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductService.Domain.Entities;
+
+namespace ProductService.Persistence.Configurations;
+
+public class ProductConfiguration :
+    IEntityTypeConfiguration<Product>
+{
+    public void Configure(
+        EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+               .HasMaxLength(200)
+               .IsRequired();
+
+        builder.Property(x => x.Description)
+               .HasMaxLength(1000);
+
+        builder.Property(x => x.Price)
+               .HasPrecision(18, 2);
+
+        builder.Property(x => x.SKU)
+               .HasMaxLength(100)
+               .IsRequired();
+
+        builder.HasOne(x => x.Category)
+               .WithMany(x => x.Products)
+               .HasForeignKey(x => x.CategoryId);
+    }
+}
